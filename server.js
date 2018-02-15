a8e2faf5e58023b819e2f459b30d38725cb67959
dan <dancolyer@icloud.com>
2018-02-15 15:00:15 +0000

    updating db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require ('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1', // <-- this address is localhost
+        host: 'postgresql-opaque-43123',
         user: 'Daniel',
         password: '',
         database: 'smart-brain'
